2c335cfae402c4d20fd81614afc192bd155abd08
Wiktor Kania <roller47@wp.pl>
2021-06-12 19:12:33 +0200

    fixed formatting

--- webpp/server.cpp
+++ webpp/server.cpp
@@ -23,13 +23,13 @@ static int (*__listen)(int, int) = listen;
 #define HTTP_404 "HTTP/1.0 404 Not Found\r\n\r\n"
 
 static std::vector<std::string> tokenize(std::string &str, char delim) {
-	std::size_t start;
-	std::size_t end = 0;
+  std::size_t start;
+  std::size_t end = 0;
   std::vector<std::string> out;
-	while ((start = str.find_first_not_of(delim, end)) != std::string::npos) {
-		end = str.find(delim, start);
-		out.push_back(str.substr(start, end - start));
-	}
+  while ((start = str.find_first_not_of(delim, end)) != std::string::npos) {
+    end = str.find(delim, start);
+    out.push_back(str.substr(start, end - start));
+  }
   return out;
 }
 
